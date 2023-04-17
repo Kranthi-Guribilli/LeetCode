026a042556a5814f65cee13c5daa212a2237cf9b
Kranthi Guribilli <kranthiguribilli@gmail.com>
2023-04-17 22:35:56 +0530

    Added solution - LeetHub

---
+++ Number of Unique Paths - GFG/number-of-unique-paths.cpp
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+//Initial template for C++
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template in C++
+
+class Solution
+{
+    public:
+    //Function to find total number of unique paths.
+    int NumberOfPath(int a, int b)
+    {
+        //code here
+        vector<int>prev(b,0);
+        for(int i=0;i<a;i++){
+            vector<int>temp(b,0);
+            for(int j=0;j<b;j++){
+                if(i==0 && j==0){
+                    temp[j]=1;
+                    continue;
+                }
+                int up=0,left=0;
+                if(i>0){
+                    up=prev[j];
+                }
+                if(j>0){
+                    left=temp[j-1];
+                }
+                temp[j]=up+left;
+            }
+            prev=temp;
+        }
+        return prev[b-1];
+    }
+};
+
+
+//{ Driver Code Starts.
+int main()
+{
+    //taking total testcases
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        //taking dimensions of the matrix
+        int a,b;
+        cin>>a>>b;
+        Solution ob;
+        //calling NumberOfPath() function
+        cout << ob.NumberOfPath(a,b) << endl;
+    }
+}
+
+
+// } Driver Code Ends
